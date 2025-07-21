edbc87e654735cfd096a9ddcca33a71586db5349
HarryRMorrison <harryrmorrison@gmail.com>
2025-07-22 00:15:11 +0800

    TIMEX3 BIO tagger

--- readers.py
+++ readers.py
@@ -1,6 +1,8 @@
 from datasets import Dataset, DatasetDict
 import xml.etree.ElementTree as ET
 import os
+import spacy
+import json
 
 def read_ozrock(auto_filepath, eval_filepath):
     for filepath in [auto_filepath, eval_filepath]:
@@ -31,25 +33,70 @@ def read_ozrock(auto_filepath, eval_filepath):
     })
     return datasets, label_list, label2id, id2label
 
-def read_timeml(directory_path):
-    for folder in os.listdir(directory_path):
-        folderpath = os.path.join(directory_path, folder)
-        if os.path.isdir(folderpath):
-            for filename in os.listdir(folderpath):
-                if filename.endswith('.tml'):
-                    filepath = os.path.join(folderpath, filename)
-                    if os.path.isfile(filepath):
-                        print(f"Processing file: {filepath}")
-    # tree = ET.parse(filepath)
+def read_timeml(folderpath, intended_path):
+    data = []
+    if os.path.isdir(folderpath):
+        for filename in os.listdir(folderpath):
+            if filename.endswith('.tml'):
+                filepath = os.path.join(folderpath, filename)
+                if os.path.isfile(filepath):
+                    print(f"Processing file: {filename}")
+                    data.extend(timex3_bio_tagger(filepath))
+    label_list, label2id, id2label = get_label_list(data, label2id=True, id2label=True)
 
-    return
+    datasets = convert_to_dataset(data, label2id)
+
+    datasets.to_json(intended_path)
+
+    return datasets, label_list, label2id, id2label
+
+# Have not included the DCT in the data structure, but it can be added if needed.
+def timex3_bio_tagger(filepath):
+    tree = ET.parse(filepath)
+    root = tree.getroot()
+    text = root.find('TEXT')
+
+    DCT = root.find('DCT').find('TIMEX3').get('value')
+
+    nlp = spacy.load("en_core_web_sm")
+    nlp.add_pipe("sentencizer")
+
+    data, sentence = [], {"tokens": [], "ner_tags": []}
+
+    for node in text.iter():
+        text_tokens = nlp(node.text.strip("\n"))
+        if isinstance(node.tag, str):
+            if node.tag == 'TIMEX3':
+                for i in range(len(text_tokens)):
+                    sentence["ner_tags"].append(f"B-"+node.attrib["type"] if i == 0 else "I-"+node.attrib["type"])
+            else:
+                sentence["ner_tags"].extend(["O"] * len(text_tokens))
+            sentence["tokens"].extend(text_tokens)
+            
+        if node.tail:
+            tail_tokens = nlp(node.tail.replace("\n\n"," ").lstrip())
+
+            if len(list(tail_tokens.sents)) > 1:
+                sents = list(tail_tokens.sents)
+                
+                sentence["tokens"].extend(sents[0])
+                sentence["ner_tags"].extend(["O"] * len(sents[0]))
+
+                data.append(sentence)
+                sentence = {"tokens": [], "ner_tags": []}
+
+                tail_tokens = sents[1]
+
+            sentence["tokens"].extend(tail_tokens)
+            sentence["ner_tags"].extend(["O"] * len(tail_tokens))
+    data.append(sentence)
+    return data
 
 def convert_to_dataset(data, label_map):
     formatted_data = {"tokens":[], "ner_tags":[]}
     for sentence in data:
-        ner_tags = [label_map[tag] for tag in sentence["ner_tags"]]
-        formatted_data["tokens"].append(sentence["tokens"])
-        formatted_data["ner_tags"].append(ner_tags)
+        formatted_data["tokens"].append([str(token) for token in sentence["tokens"]])
+        formatted_data["ner_tags"].append([label_map[tag] for tag in sentence["ner_tags"]])
     return Dataset.from_dict(formatted_data)
 
 def get_label_list(data, label2id=True, id2label=True):
